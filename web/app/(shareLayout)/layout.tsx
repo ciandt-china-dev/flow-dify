fa405b4169a945883c4eb219a2d25df870a081e6
song <songs@ciandt.com>
2024-05-16 13:16:58 +0800

    Fix siteguard under chat router issue.

--- web/app/(shareLayout)/layout.tsx
+++ web/app/(shareLayout)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { FC } from 'react'
 import GA, { GaType } from '@/app/components/base/ga'
+import SiteGuard from '../components/site-guard'
 
 const Layout: FC<{
   children: React.ReactNode
@@ -8,6 +9,7 @@ const Layout: FC<{
   return (
     <div className="min-w-[300px] h-full pb-[env(safe-area-inset-bottom)]">
       <GA gaType={GaType.webapp} />
+      <SiteGuard />
       {children}
     </div>
   )
